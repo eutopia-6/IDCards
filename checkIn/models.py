89a6de1da45c64ba20f84ae04425ccd88d2500f1
Summit <lisummit6@gmail.com>
2022-11-27 20:13:26 -0500

    -Models has been changed

--- checkIn/models.py
+++ checkIn/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class iDCards(models.Model):
+    firstName = models.CharField(max_length=20)
+    lastName = models.CharField(max_length=20)
+
+class DateTime(models.Model):
+    timeIn = models.DateTimeField("Checked In")
+    timeOut = models.DateTimeField("Checked Out")
