47e3baac9bde528aad12bdd0fe57e04368e90cda
Summit <lisummit6@gmail.com>
2023-04-07 11:29:30 -0400

    -Front-end work

--- checkIn/static/checkIn/scripts/base.js
+++
@@ -1,4 +0,0 @@
-createNewForm = ['id_name', 'id_height_cm', 'id_weight_ib', 'id_eye_color', 'id_image'];
-for(let i = 0; i < createNewForm.length; i++){
-    document.getElementById(createNewForm[i]).className = 'form-control form-control';
-}
---
+++ checkIn/static/checkIn/scripts/createnew.js
@@ -0,0 +1,4 @@
+createNewForm = ['id_name', 'id_height_cm', 'id_weight_ib', 'id_eye_color', 'id_image'];
+        for(let i = 0; i < createNewForm.length; i++){
+            document.getElementById(createNewForm[i]).className = 'form-control form-control';
+        }
